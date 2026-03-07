21abccc98e919b9dd5d14ce9fc82be99e738f2f6
skeplys83 <martin.hustoles@gmail.com>
2026-03-07 19:46:07 +0100

    updated plugin

--- jumpandrun-plugin/scripts/deploy.sh
+++ jumpandrun-plugin/scripts/deploy.sh
@@ -42,10 +42,14 @@ mkdir -p ~/.ssh
 printf '%s\n' "$JNR_SFTP_SSH_KEY" > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 
+echo "test1"
+
 # Add server host key
 if ! ssh-keyscan -p "$JNR_SFTP_PORT" "$JNR_SFTP_HOST" >> ~/.ssh/known_hosts 2>/dev/null; then
   echo "Warning: ssh-keyscan failed for $JNR_SFTP_HOST:$JNR_SFTP_PORT; continuing with strict host key accept-new" >&2
 fi
+
+echo "test2"
 
 # Upload plugin
 scp -o StrictHostKeyChecking=accept-new -i ~/.ssh/id_ed25519 -P "$JNR_SFTP_PORT" "$JAR_PATH" "$JNR_SFTP_USER@$JNR_SFTP_HOST:/plugins/$JNR_PLUGIN_FILE_NAME"
