c8c889fc99e575431e78cbf6d2f6b8c73563d0b2
AdrianJBecker <adrian.julian.becker@gmail.com>
2026-03-08 02:54:37 +0100

    Name Command geändert

--- jumpandrun-plugin/src/main/java/org/pixelcampus/jumpnrun/Jumpnrun.java
+++ jumpandrun-plugin/src/main/java/org/pixelcampus/jumpnrun/Jumpnrun.java
@@ -1,6 +1,8 @@
 package org.pixelcampus.jumpnrun;
 
+import Events.Schild;
 import commands.ParkourWerte;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Jumpnrun extends JavaPlugin {
@@ -8,6 +10,7 @@ public final class Jumpnrun extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("parkourwerte").setExecutor(new ParkourWerte());
+        getServer().getPluginManager().registerEvents(new Schild(), this);
     }
 
     @Override
