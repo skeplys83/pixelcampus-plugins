5b317d70f6e0d86716a74e24bc097c9945d49721
AdrianJBecker <adrian.julian.becker@gmail.com>
2026-03-08 04:28:33 +0100

    Name Command geändert

--- jumpandrun-plugin/src/main/java/Events/Schild.java
+++ jumpandrun-plugin/src/main/java/Events/Schild.java
@@ -1,8 +1,11 @@
 package Events;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -13,7 +16,7 @@ import java.util.List;
 public class Schild implements Listener {
 
 
-    @EventHandler
+    //@EventHandler
     public void onSignPlace(BlockPlaceEvent event){
 
     if(event.getBlockPlaced().getType() == Material.OAK_SIGN ){
@@ -25,12 +28,13 @@ public class Schild implements Listener {
     @EventHandler
     public void onSignWrite(SignChangeEvent event){
         TextComponent line = (TextComponent)event.line(0);
-        String firstLine = line.content().lines().findFirst().get();
+        List<String> lines = line.content().lines().toList();
+        Player player = event.getPlayer();
 
-        if(firstLine.equals("[1. Platz]")){
-            event.getPlayer().kick();
-        }
-        else{
+        if(lines.get(0).equals("[1. Platz]")){
+            String score = player.getName() + PlaceholderAPI.setPlaceholders(player, "%parkour_course_record_" + lines.get(1) + "_time%");
+            event.line(2, Component.text(score));
+        } else{
             event.getPlayer().sendMessage("falsch");
         }
 
