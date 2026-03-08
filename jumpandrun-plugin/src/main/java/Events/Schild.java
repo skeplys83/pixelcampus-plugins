1b62ccf7dd626db0f7d66e01aba55d7be890b73a
AdrianJBecker <adrian.julian.becker@gmail.com>
2026-03-08 05:00:00 +0100

    Name Command geändert

--- jumpandrun-plugin/src/main/java/Events/Schild.java
+++ jumpandrun-plugin/src/main/java/Events/Schild.java
@@ -35,10 +35,10 @@ public class Schild implements Listener {
         List<String> lines = line.content().lines().toList();
         Player player = event.getPlayer();
 
-        log.atInfo().log(lines.toArray());
+        lines.forEach(x -> log.atInfo().log(line));
 
         if(lines.get(0).equals("[1. Platz]") && lines.size() != 2){
-            String score = player.getName() + PlaceholderAPI.setPlaceholders(player, "%parkour_course_record_" + "" + "_time%"); //lines.get(1)
+            String score = player.getName() + PlaceholderAPI.setPlaceholders(player, "%parkour_course_record_" + "Beispiel" + "_time%"); //lines.get(1)
 
             try {
                 event.line(2, Component.text(score));
